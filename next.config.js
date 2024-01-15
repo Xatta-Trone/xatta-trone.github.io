46558bebf78578ed1357917bace0dec77d71e890
Monzurul Islam <monzurul.ce.buet@gmail.com>
2024-01-15 11:21:15 +0900

    update

--- next.config.js
+++ next.config.js
@@ -12,8 +12,8 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/xatta-trone.github.io",
-  assetPrefix: "/xatta-trone.github.io/",
+  basePath: "",
+  assetPrefix: "/",
 
   /**
    * Disable server-based image optimization. Next.js does not support
